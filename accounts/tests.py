e10d872f53371dc70aaf7db1ae90276e65843009
pjelelhml <paulo3385@hotmail.com>
2021-06-16 23:09:24 -0400

    fazendo alguns testes

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,32 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 # Create your tests here.
+# TDD
+
+User = get_user_model
+
+
+class UserTestCast(TestCase):
+
+    def setUp(self):  # Python's builtin unittest
+        user_a = User(username='cfe', email='cfe@invalid.com')
+        # User.objects.create()
+        # User.objects.create_user()
+        user_a_pw = 'some_123_password'
+        self.user_a_pw = user_a_pw
+        user_a.is_staff = True
+        user_a.is_superuser = True
+        user_a.save()
+        user_a.set_password(user_a_pw)
+        print(user_a.id)
+
+    def test_user_exists(self):
+        user_count = User.objects.all().count()
+        self.assertEqual(user_count, 1)
+        self.assertNotEqual(user_count, 0)
+
+    def test_user_password(self):
+        user_qs = User.objects.filter(username__iexact="cfe")
+        user_exists = user_qs.exists() and user_qs.count() == 1
+        self.assertTrue(user_exists)
