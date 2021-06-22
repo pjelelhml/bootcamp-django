1145667bd87cbb7ed063643b81d32aca7c2d3fb8
pjelelhml <paulo3385@hotmail.com>
2021-06-22 01:29:34 -0400

    some tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -10,7 +10,7 @@ User = get_user_model
 class UserTestCast(TestCase):
 
     def setUp(self):  # Python's builtin unittest
-        user_a = User(username='cfe', email='cfe@invalid.com')
+        user_a = User(username='phml', email='cfe@invalid.com')
         # User.objects.create()
         # User.objects.create_user()
         user_a_pw = 'some_123_password'
@@ -19,7 +19,7 @@ class UserTestCast(TestCase):
         user_a.is_superuser = True
         user_a.save()
         user_a.set_password(user_a_pw)
-        print(user_a.id)
+        self.user_a = user_a
 
     def test_user_exists(self):
         user_count = User.objects.all().count()
@@ -27,6 +27,8 @@ class UserTestCast(TestCase):
         self.assertNotEqual(user_count, 0)
 
     def test_user_password(self):
-        user_qs = User.objects.filter(username__iexact="cfe")
-        user_exists = user_qs.exists() and user_qs.count() == 1
-        self.assertTrue(user_exists)
+        # user_qs = User.objects.filter(username__iexact="cfe")
+        # user_exists = user_qs.exists() and user_qs.count() == 1
+        # self.assertTrue(user_exists)
+        # user_a = user_qs.first()
+        self.assertTrue(self.user_a.check_password(self.user_a_pw))
